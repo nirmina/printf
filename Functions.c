c0983a443e3dd8db45212886ead8740b724b7c85
nirmina <nirminahiani12@gmail.com>
2023-09-22 18:21:27 -0700

    hard task

--- Functions.c
+++ Functions.c
@@ -17,7 +17,6 @@ int _putchar(char c)
  */
 int handle_string(char *str)
 {
-<<<<<<< HEAD
 int i = 0;
 if (str == NULL)
 {
@@ -29,21 +28,6 @@ _putchar(str[i]);
 i++;
 }
 return (i);
-=======
-	int i = 0;
-
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
-	return (i);
->>>>>>> 76b63bfe47c6494190af938436789f7c5a733bb8
 }
 /**
  * handle_int - prints int
@@ -52,7 +36,6 @@ return (i);
  */
 int handle_int(int num)
 {
-<<<<<<< HEAD
 int count = 0;
 int digit;
 if (num < 0)
@@ -98,40 +81,5 @@ int count;
 va_start(args, format);
 count = vsprintf(buffer, format, args);
 va_end(args);
-return count;
-=======
-	int count = 0;
-	int digit;
-
-	if (num < 0)
-	{
-		_putchar('-');
-		count++;
-		num = -num;
-	}
-	if (num == 0)
-	{
-		_putchar('0');
-		count++;
-	}
-	else
-	{
-		int rev = 0;
-
-	while (num > 0)
-	{
-		digit = num % 10;
-		rev = rev * 10 + digit;
-		num /= 10;
-	}
-	while (rev > 0)
-	{
-		_putchar(rev % 10 + '0');
-		count++;
-		rev /= 10;
-	}
-	}
-	return (count);
-	
->>>>>>> 76b63bfe47c6494190af938436789f7c5a733bb8
+return (count);
 }
--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <limits.h>
 #include "main.h"
-
 /**
  * main - Entry point
  *
@@ -10,7 +9,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 int len, len2;
 len = _printf("%d", INT_MAX);
 len2 = printf("%d", INT_MAX);
@@ -22,18 +20,4 @@ fflush(stdout);
 return (1);
 }
 return (0);
-=======
-	int len, len2;
-
-	len = _printf("%d", 1024);
-	len2 = printf("%d", 1024);
-	fflush(stdout);
-	if (len != len2)
-	{
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
-	return (0);
->>>>>>> 76b63bfe47c6494190af938436789f7c5a733bb8
 }
